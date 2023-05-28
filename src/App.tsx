7c1a55605acc4385b6f028cd0ed067b42bc31eb9
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-05-28 16:08:04 -0700

    lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
 import words from "./wordList.json"
+import { HangmanDrawing } from './HangmanDrawing copy'
+import { HangManWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(()=> {
     return words[Math.floor(Math.random() * words.length)]
   })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   console.log(wordToGuess)
-return <h1>Hi</h1>
-  
+
+return (
+<div 
+style={{
+maxWidth: "800px",
+display: "flex",
+flexDirection: "column",
+gap: "2rem",
+margin: "0 auto",
+alignItems: "center"
+}}
+> 
+<div style={{ fontSize: "2rem", textAlign: "center"}}>
+  Lose 
+  Win
+</div>
+<HangmanDrawing/>
+<HangManWord/>
+<Keyboard />
+</div>
+)
 }
 
 export default App
