134d54220dba690bc0ce07207cba1ccebc90d55a
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-05-29 11:01:56 -0700

    letters

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,28 @@
-export function HangManWord(){
-    
-}
+export function HangManWord() {
+    const word = "test";
+    const guessedLetters = ["t", "e", "g"]
+    return (
+      <div
+        style={{
+          display: "flex",
+          gap: ".25em",
+          fontSize: "6rem",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+          fontFamily: "monospace"
+        }}
+      >
+        {word.split("").map((letter, index) => {
+          return <span style={{ borderBottom: ".1em solid black" }}>
+            <span style={{
+                visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+            }}
+            > 
+            {letter} 
+            </span>
+            </span>;
+        })}
+      </div>
+    );
+  }
+  
