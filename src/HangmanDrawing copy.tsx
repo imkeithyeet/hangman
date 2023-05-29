eacfc4c85b3c5add91f1c730f948ccc6327d78bd
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-05-28 22:56:57 -0700

    liness

--- src/HangmanDrawing copy.tsx
+++ src/HangmanDrawing copy.tsx
@@ -1,6 +1,20 @@
+const HEAD = (
+    <div style ={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "100%",
+        border: "10px solid black",
+        position: "absolute",
+        top: "50px",
+        right: "-30px",
+
+    }}/>
+)
+
 export function HangmanDrawing(){
     return (
     <div style={{ position: "relative"}}>
+        {HEAD}
          <div 
         style={{ 
             height: "50px", 
